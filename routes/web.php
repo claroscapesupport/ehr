2e5235e14ff5916a387379400b8d8fd1f813ce52
Duncan Cavalier <duncan.cavalier@claroscape.com>
2018-12-05 20:42:00 +0000

    update

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'PagesController@index');
 Route::get('/about', 'PagesController@about');
 Route::get('/services', 'PagesController@services');
+Route::get('/ehr', 'PagesController@ehr');
 Route::resource('posts','PostsController');
 
 Auth::routes();
