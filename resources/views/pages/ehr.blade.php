34df85bb8831bb662c0c2fb85ba8292836453385
Duncan Cavalier <duncan.cavalier@claroscape.com>
2018-12-06 14:47:32 +0000

    Commit

--- resources/views/pages/ehr.blade.php
+++ resources/views/pages/ehr.blade.php
@@ -5,6 +5,19 @@
 <p>This is the electronic health records page</p>
 
 
+<script type="text/javascript">
+    var fhirClient = FHIR.client({
+    serviceUrl: window.sessionStorage.smartServiceUrl,
+    patientId: window.sessionStorage.smartPatientId,
+    auth: {
+        token: window.sessionStorage.smartAuthToken,
+        type: window.sessionStorage.smartAuthType
+    }
+    });
+ 
+    var patient = fhirClient.patient;
+</script>
+
 
 
 <table id="obsTable">
