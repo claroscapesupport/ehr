a1e94c5da7d1253b93f3d21b857a916fd36d248f
Duncan Cavalier <duncan.cavalier@claroscape.com>
2018-12-06 15:02:12 +0000

    commit

--- public/js/app.js
+++ public/js/app.js
@@ -53986,6 +53986,7 @@ function FhirClient(p) {
     };
 
     if (!client.server.serviceUrl || !client.server.serviceUrl.match(/https?:\/\/.+[^\/]$/)) {
+      console.log("client server: " + client.server.serviceUrl);
       throw "Must supply a `server` property whose `serviceUrl` begins with http(s) " + 
         "and does NOT include a trailing slash. E.g. `https://fhir.aws.af.cm/fhir`";
     }
