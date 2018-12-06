76873c70b8d90c0a7bbfb5e93ca988c69d7946ca
Duncan Cavalier <duncan.cavalier@claroscape.com>
2018-12-06 14:43:42 +0000

    Update ehr.blade.php

--- resources/views/pages/ehr.blade.php
+++ resources/views/pages/ehr.blade.php
@@ -4,27 +4,9 @@
 <h1>{{$title}}</h1>
 <p>This is the electronic health records page</p>
 
-<script type="text/javascript">
-    FHIR.oauth2.ready(function (fhirClient) {
-        var patient = fhirClient.patient;
-        $.when(patient.read())
-            .done(function (p) {
-                var name = p.name[0];
-                var formattedName = name.given[0] + " " + name.family;
-                $("#patientName").text(formattedName);
-            });
-        $.when(patient.api.search({type: "Observation", query: {code: '8302-2'}, count: 50}))
-            .done(function (obsSearchResults) {
-                obsSearchResults.data.entry.forEach(function (obs) {
-                    var obsRow = "<tr><td>" + obs.resource.effectiveDateTime + "</td>" + "<td>" +
-                        obs.resource.valueQuantity.value + obs.resource.valueQuantity.unit + "</td></tr>"
-                    $("#obsTable").append(obsRow);
-                });
-            });
-    });
-</script>
 
-<h2 id="patientName"></h2>
+
+
 <table id="obsTable">
     <tr>
         <th>Date</th>
